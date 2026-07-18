79f17b1479527cf14ea371fb143742100a898789
Aniket Pravesh Singh <142985263+aniket1920@users.noreply.github.com>
2026-07-18 19:35:17 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (76.45%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int n = s.length();
+        int m = t.length();
+        if(n!=m){
+            return false;
+        }
+        vector<int> arr(26,0);
+        for(int i = 0; i<n; i++){
+            arr[s[i]-'a']++;
+            arr[t[i]-'a']--;
+        }
+        for(int i = 0; i<26; i++){
+            if(arr[i]!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
